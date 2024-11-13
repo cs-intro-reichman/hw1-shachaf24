e2b0686a9c8f89442b0401e16c17851eb625533e
Shachaf Amar <shachafmeir.amar@post.runi.ac.il>
2024-11-13 11:33:29 +0200

    bill3

--- Bill3.java
+++ Bill3.java
@@ -4,7 +4,7 @@ public class Bill3 {
 		// sumBill gets the sum provied as an argument  
 	    double sumBill =  Double.parseDouble(args[3]);
 		double sumForPerson = Math.ceil(sumBill/3);
-		System.out.println("Dear " + args[2] + ", " + args[1] + ", " +
-		 args[0] + ": pay " + sumForPerson + " Shekels each");
+		System.out.println("Dear " + args[0] + ", " + args[1] + ", " +
+		 args[2] + ": pay " + sumForPerson + " Shekels each");
 	}
 }
